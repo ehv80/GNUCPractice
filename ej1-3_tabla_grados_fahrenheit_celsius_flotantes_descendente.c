f78ff6389ff40ee5a7d63fd82d20cb22ded3f780
Ezequiel Hernán Villanueva <ezequiel.villanueva@gmail.com>
2018-05-02 19:43:27 -0300

    Add files via upload

---
+++ ej1-3_tabla_grados_fahrenheit_celsius_flotantes_descendente.c
@@ -0,0 +1,50 @@
+/* Ejemplo de la Sección 1.3 La Proposición For
+   del Libro del Lenguaje de Programación C, pag. 14
+   Ejercicio 1.5
+
+   Fórmula Matemática:  ºC = (5.0/9.0)*(ºF - 32.0)
+   
+   Imprime la Tabla de Temperaturas en Grados Fahrenheit - Celsius 
+   para fahr = 300, ... , 20, 0
+   es decir en orden descendente.
+
+   Produce los mismos resultados pero ciertamente se ve diferente
+*/
+
+#include <stdio.h>
+
+main()
+{
+	// float fahr, celsius;
+	// int lower, upper, step;
+	// Eliminación de la mayoría de las variables
+	
+	int fahr; 
+	// solo permanece la variable fahr y la hemos hecho del tipo de dato int
+
+	printf("\nLa Tabla de Temperaturas en Grados Fahrenheit y en Celsius\n");
+	printf("Para 300 >= Fahrenheit >= 0 es decir, en orden descendente \n");
+	printf("Con un decremento de 20º Fahrenheit por cada línea \n");
+	printf("\nºF\tºC\n");
+	
+	//for( fahr = 0 ; fahr <= 300 ; fahr = fahr + 20 )
+	for( fahr = 300 ; fahr >= 0 ; fahr = fahr - 20 )
+	{	
+		printf("%3d %6.1f\n", fahr, (5.0/9.0) * (fahr - 32.0));
+	} 
+	/* 
+        Los límites inferior y superior y el tamaño del incremento 
+        solo aparecen como constantes dentro de la proposición for. 
+
+	La expresión matemática que calcula la temperatura en grados Celsius
+        ahora aparece como tercer argumento de la función printf() de stdio.h
+        en lugar de una declaración de asignación separada.
+
+        Como regla general: en cualquier contexto en el que se permita utilizar
+        el valor de una variable de algún tipo de dato, es posible utilizar una 
+        expresión matemática más complicada que resulte en un valor 
+        de ese mismo tipo de dato.
+        
+	Se obtiene una solución más estética.
+	*/
+}
