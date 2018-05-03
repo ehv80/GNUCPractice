0c6961c7ef6cc284d592fb7b71e5efd546452371
Ezequiel Hernán Villanueva <ezequiel.villanueva@gmail.com>
2018-05-03 20:28:52 -0300

    Add files via upload

---
+++ ej1-5-1_ESCaracteres_copia_archivos-Ejercicio_1-7.c
@@ -0,0 +1,15 @@
+/* Ejemplo de la Sección 1.5 Entrada y salida de caracteres
+   1.5.1 Copia de archivos 
+   Ejercicio 1-7
+   del Libro del Lenguaje de Programación C, pag. 18
+   
+   En este ejercicio se imprime el valor de la CONSTANTE SIMBÓLICA
+   EOF definida en stdio.h
+*/
+
+#include <stdio.h>
+
+main()
+{
+	printf("\nLa CONSTANTE SIMBÓLICA EOF definida en stdio.h vale: %d \n", EOF);
+}
