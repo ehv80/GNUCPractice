3eecfa30c8cca1b96baf489a35193151e08ced85
Ezequiel Hernán Villanueva <ezequiel.villanueva@gmail.com>
2018-05-03 20:17:37 -0300

    Add files via upload

---
+++ ej1-5-1_ESCaracteres_copia_archivos-Ejercicio_1-6.c
@@ -0,0 +1,58 @@
+/* Ejemplo de la Sección 1.5 Entrada y salida de caracteres
+   1.5.1 Copia de archivos 
+   Ejercicio 1-6
+   del Libro del Lenguaje de Programación C, pag. 18
+
+   El ejemplo más sencillo es un programa que copia la entrada en la salida,
+   de a un caracter a la vez puede escribirse de modo más conciso.
+
+   En Lenguaje C, cualquier asignación, tal como
+
+	c = getchar()
+
+   es una expresión y tiene un valor, el del lado izquierdo 
+   luego de la asignación. Esto significa que una asignación 
+   puede aparecer como parte de una expresión más larga. 
+   La asignación de un caracter a la variable entera c 
+   la colocamos dentro de la sección de prueba del ciclo while,
+   así solo hay una referencia a la función getchar() de stdio.h
+   y se reduce el código fuente del programa.
+
+   Los paréntesis que están
+
+	( c = getchar() )
+
+   son necesarios porque la precedencia del operador de comparación !=
+   es más alta que la del operador de asignación de variable =
+   lo que significa que en ausencia de estos paréntesis 
+   la operación de comparación != se realizaría antes de
+   la operación de asignación de variable =
+   De esta manera, la proposición
+
+	c = getchar() != EOF
+
+   es equivalente a
+
+	c = ( getchar() != EOF )
+
+   y tiene el efecto indeseable de hacer que c sea 0 o 1
+   dependiendo de si la llamada a getchar() encontró EOF
+   
+   En este ejercicio se verifica si la expresión getchar() != EOF es 0 o 1
+*/
+
+#include <stdio.h>
+
+main()
+{
+	int c;
+	//c = getchar();
+	// != operador de comparación "no igual a" "distinto de"
+	//while ( ( c = getchar() )  != EOF )
+	while (  c = ( getchar() != EOF ) )
+	{
+		//putchar(c);	No se llega a inicializar la variable entera c
+		printf("\nLa expresión getchar() != EOF vale: %d \n", c);
+		//c = getchar();
+	}
+}
