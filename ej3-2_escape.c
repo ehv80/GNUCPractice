ba6b7f7694b0089ad10fd9f73aeb73be6efcba0f
Ezequiel Hernán Villanueva <ezequiel.villanueva@gmail.com>
2022-07-28 17:34:30 -0300

    Add files via upload

---
+++ ej3-2_escape.c
@@ -0,0 +1,134 @@
+/**
+ * Archivo / @File:                             ej3-2_escape.c 
+ *
+ * Fecha / @Date:                               Jueves 28 de Julio de 2022 /  Thursday July 28, 2022
+ *
+ * Última Modificación / Last Modification:     Jueves 28 de Julio de 2022 /  Thursday July 28, 2022 
+ *
+ * Autor / @Author:                             Ezequiel Hernán Villanueva
+ *
+ * 
+ * [ Spanish ]
+ *
+ * 3.4 Switch 
+ * 
+ * 
+ *
+ * [ English ]
+ *
+ * 3.4 Switch
+ * ...
+ *
+ *
+ *
+ *
+ *
+ * Some source code files with examples from The Book
+ * "The C Language Programming, Second Edition
+ * By Brian W. Kernighan & Dennis M. Ritchie"
+ * a Compiled Programming Language.
+ * Use "gcc - GNU project C and C++ compiler"
+ * for various GNU\Linux based Operative Systems.
+ *
+ * Edited a little time ago!
+ *
+ * You can Copy / Edit / Change / Distribute
+ * GNU GPL License Software Version 3.0
+ *
+ * For Educational Purpose.
+ *
+ */
+
+#include <stdio.h>
+//#include <string.h>
+//#include <limits.h>
+//#include <float.h>
+//#include <math.h>
+
+// Declaración de CONSTANTES SIMBÓLICAS para evitar LOS NÚMEROS MÁGICOS
+// Declaration of SYMBOLIC CONSTANTS for avoid THE MAGICS NUMBERS
+//#define MAXLINE 32767
+		  	/* 
+                        *  Tamaño máximo de la línea de entrada 
+                        *  "tanto texto como sea posible"
+                        *  para un int de 16 bits el rango de valores es [ -32768, +32767 ] pag. 10
+                        *
+                        *  Maximun length of input textline 
+                        *  "as much text as possible"
+                        *  for a 16-bit int the range of values is [-32768, +32767] p. 10
+                        */
+
+// Declaración de las VARIABLES EXTERNAS
+// Declaration of the EXTERN VARIABLES
+//int  max;                // máxima longitud de una línea vista hasta el momento
+//char line[MAXLINE];     // línea de entrada actual
+//char longest[MAXLINE];  // línea mas larga se guarda aquí
+
+// Declaración de los PROTOTIPOS de las FUNCIONES   getaline   y     copy
+// Declaration of the PROTOTYPES OF THE FUNCTIONS             and
+// int getaline( char line[], int maxline );
+//int getaline( void );
+//int hextoint( char s[] ); 
+
+//void copy( char to[], char from[] );
+//void copy( void );
+
+/*  
+*   Definición de la FUNCIÓN: 	escape( cha s[], char t[] )
+*  
+*   Convierte caracteres como nueva línea y tabulación en secuencias de escape visibles como \n y \t
+*   mientras copia la cadena t a s, usando un switch
+*/
+
+int escape( char s[] , char t[] )
+{
+	int i = 0;
+	while ( t[i] = s[i] )
+	{
+		switch(t[i])
+		{
+			case '\n' : t[i] = '\n';
+			break;
+			case '\t' : t[i] = '\t';
+			break;
+			default: printf("Caso por defecto no contemplado");
+			break;
+		}
+		i++;
+	}
+	return 0;
+}
+
+
+// Programa Principal
+// Main Program
+// Cuenta dígitos, espacios blancos y otros 
+int main( int c, char * argv[] )
+{
+	char s[10], t[10];
+	int i;
+	// incializo los arreglos de 10 caracteres
+	for( i=0 ; i < 10 ; i++ )
+	{
+		s[i] = i;
+		t[i] = i;
+		if( i == 2 ) // en la 2da posición tendrá nueva linea
+		{
+			s[i] = '\n';
+			t[i] = '\n'; 
+		}
+		if( i == 4) // en la cuarta posición tendrá tabulación
+		{
+			s[i] = '\t';
+			t[i] = '\t';
+		}
+	}
+	printf("\nArreglo de caracters t: %s\n" , t);
+	printf("\nArreglo de caracteres s: %s\n", s);
+
+	printf("Se aplica la función escape(s,t) ");
+	int resultado = escape ( s, t );
+	printf(" Y el resultado es: %d", resultado );
+
+	return 0;
+}
